4f243bf25168f6eb735f9aa2adb40a82aef10eda
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-09-14 06:36:57 +0300

    File content is added

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+// import node_modules
+// http module helps us create a server 
+var http = require('http');
 
+http.createServer(function (req, res) {
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.end('Hello World!');
+
+}).listen(8080);
