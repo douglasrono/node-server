c736ad39354d3a1fbaa7eac52ecb3e370d531ff6
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-10-06 20:10:31 +0300

    added date display

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 
 const http = require('http'),
-      fs = require('fs);
+      fs = require('fs)
+       dt = require('./appDate');
 
 http.createServer((req, res) => {
   //Read index.html file
   fs.readFile('index.html', (err, data) =>{
     // Write headers to the file
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+    res.write(data + dt);
     return res.end();
   });
 
