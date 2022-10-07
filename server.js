0c83ebf48738a23c4e518c3ac8ffb96aec78231f
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-10-07 18:26:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 
-const http = require('http'),
-      fs = require('fs)
-       dt = require('./appDate');
-
+const http = require('http');
+const fs = require('fs);
+       
 http.createServer((req, res) => {
-  //Read index.html file
-  fs.readFile('index.html', (err, data) =>{
     // Write headers to the file
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data + dt);
+    res.write("hello There!");
+    res.write("I'm from the server!");
+    res.write("GOODBYE!");
     return res.end();
   });
 
-}).listen(8080);
+}).listen(8080); // access the server on port 8000
