9658a0df37140594f6cfb9779d8bdb863441f189
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-10-08 05:21:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 const http = require('http');
 const fs = require('fs);
+const url = require('url');
+const path = require('os');
 
  //create a server object:  
 http.createServer((req, res) => {
@@ -8,13 +10,14 @@ http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     
    //Read html file
-    fs.readFile('index.html', function(err, resData) {
+    fs.readFile('index.html',(err, resData) => {
    // Write html content to the response 
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    if(err) throw err;
     res.write(resData);
     return res.end();
   });
- server.on('connection',  () =>{
+//Listen to connection event
+ http.on('connection',  () =>{
   console.log('Connection Created!');
 
 }).listen(8080); // access the server on port 8000
